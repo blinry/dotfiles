832e52d0da9d172cf08879494c5e0f1f31a220ec
Sebastian Morr <sebastian@morr.cc>
2014-02-09 09:56:21 +0100

    [fish] Don't display Git branch in the prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -2,15 +2,12 @@ function fish_prompt
     echo -n (set_color blue --bold)
     echo -n (pwd | sed "s|^$HOME|~|")
     echo -n (set_color green --bold)
-    echo -n " "
     if git rev-parse --git-dir > /dev/null 2>&1
-        echo -n (git rev-parse --abbrev-ref HEAD)
         git diff --quiet HEAD ^&-
         if test $status = 1
             echo -n (set_color yellow --bold)
             echo -n "'"
         end
-
-        echo -n " "
     end
+    echo -n " "
 end
