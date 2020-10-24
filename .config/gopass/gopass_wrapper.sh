3c8de47788e94bab04b476612f4c4958bc18b9a3
Sebastian Morr <sebastian@morr.cc>
2020-10-24 11:36:01 +0200

    [gopass] gopass now requires "show", and uses "gopass-jsonapi"

--- .config/gopass/gopass_wrapper.sh
+++ .config/gopass/gopass_wrapper.sh
@@ -10,6 +10,6 @@ fi
 export PATH="$PATH:/usr/local/bin" # required on MacOS/brew
 export GPG_TTY="$(tty)"
 
-/usr/bin/gopass jsonapi listen
+/usr/bin/gopass-jsonapi listen
 
 exit $?
--- .offlineimap.py
+++ .offlineimap.py
@@ -3,4 +3,4 @@ from subprocess import check_output
 
 
 def get_pass(account):
-    return check_output("gopass " + account, shell=True).splitlines()[0]
+    return check_output("gopass show " + account, shell=True).splitlines()[0]
