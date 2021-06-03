d9b1889c11a73eb3f897ba38745a20cfa6bab9e7
blinry <sebastian@morr.cc>
2021-06-03 14:45:01 +0200

    [vim] Reformat init.lua with lua-format

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,7 @@
 local function map(mode, lhs, rhs, opts)
-  local options = {noremap = true}
-  if opts then options = vim.tbl_extend('force', options, opts) end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+    local options = {noremap = true}
+    if opts then options = vim.tbl_extend('force', options, opts) end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 vim.g.mapleader = "," -- Use comma as a <Leader>.
@@ -9,29 +9,29 @@ vim.g.mapleader = "," -- Use comma as a <Leader>.
 -- Initialize Paq, and let it manage itself.
 vim.cmd('packadd paq-nvim')
 local paq = require('paq-nvim').paq
-paq {'savq/paq-nvim', opt=true}
+paq {'savq/paq-nvim', opt = true}
 
 -- LSP quickstart.
 paq {'neovim/nvim-lspconfig'}
-  local custom_lsp_attach = function()
-        local opts = {}
-        map('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-        map('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-        map('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-        map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-        map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-        map('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-        map('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-        map('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-        map('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-        map('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-        map('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-        map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-        map('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-        map('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-        map('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-        map('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-        map("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+local custom_lsp_attach = function()
+    local opts = {}
+    map('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    map('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    map('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    map('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    map('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    map('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    map('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    map('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    map('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    map('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+    map('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+    map('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+    map('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+    map("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 
     -- Use LSP as the handler for omnifunc.
     --    See `:help omnifunc` and `:help ins-completion` for more information.
@@ -39,130 +39,86 @@ paq {'neovim/nvim-lspconfig'}
 
     -- For plugins with an `on_attach` callback, call them here. For example:
     -- require('completion').on_attach()
-  end
+end
 
-  require('lspconfig').solargraph.setup{
-    on_attach = custom_lsp_attach
-  }
-  require('lspconfig').sumneko_lua.setup{
+require('lspconfig').solargraph.setup {on_attach = custom_lsp_attach}
+require('lspconfig').sumneko_lua.setup {
     cmd = {"lua-language-server"},
     root_dir = require('lspconfig').util.root_pattern(".git", "init.lua"),
     settings = {
-      Lua = {
-        runtime = {
-          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-          version = 'LuaJIT',
-          -- Setup your lua path
-          path = vim.split(package.path, ';'),
-        },
-        diagnostics = {
-          -- Get the language server to recognize the `vim` global
-          globals = {'vim'},
-        },
-        workspace = {
-          -- Make the server aware of Neovim runtime files
-          library = {
-            [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-            [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-          },
-        },
-        -- Do not send telemetry data containing a randomized but unique identifier
-        telemetry = {
-          enable = false,
-        },
-      }
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+                -- Setup your lua path
+                path = vim.split(package.path, ';')
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {'vim'}
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true}
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {enable = false}
+        }
     }
-  }
-  require"lspconfig".efm.setup {
-
-  init_options = {documentFormatting = true},
-
-  filetypes = {"lua"},
-
-  settings = {
-
-      rootMarkers = {".git", "init.lua"},
-
-      languages = {
-
-          lua = {
-
-              {
-
-                  formatCommand = "lua-format -i --no-keep-simple-function-one-line --no-break-after-operator --column-limit=150 --break-after-table-lb",
-
-                  formatStdin = true
-
-              }
-
-          }
-
-      }
-
-  }
-
 }
-  require('lspconfig').vimls.setup{}
-  require('lspconfig').tsserver.setup{
-      on_attach = custom_lsp_attach
-  }
-  require('lspconfig').html.setup{
-      cmd = { "vscode-html-languageserver", "--stdio" }
-  }
+require"lspconfig".efm.setup {
+    init_options = {documentFormatting = true},
+    filetypes = {"lua"},
+    settings = {
+        rootMarkers = {".git", "init.lua"},
+        languages = {
+            lua = {
+                {
+                    formatCommand = "lua-format -i --no-keep-simple-function-one-line --no-break-after-operator --column-limit=150 --break-after-table-lb",
+                    formatStdin = true
+                }
+            }
+        }
+    }
+}
+require('lspconfig').vimls.setup {}
+require('lspconfig').tsserver.setup {on_attach = custom_lsp_attach}
+require('lspconfig').html.setup {cmd = {"vscode-html-languageserver", "--stdio"}}
 
-  vim.api.nvim_command[[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
+vim.api.nvim_command [[autocmd BufWritePre *.lua lua vim.lsp.buf.formatting_sync()]]
 
 -- Fix colorschemes.
 paq {'folke/lsp-colors.nvim'}
 
 -- Completions
 paq {'hrsh7th/nvim-compe'}
-  require'compe'.setup {
-    enabled = true;
-    autocomplete = true;
-    debug = false;
-    min_length = 1;
-    preselect = 'enable';
-    throttle_time = 80;
-    source_timeout = 200;
-    incomplete_delay = 400;
-    max_abbr_width = 100;
-    max_kind_width = 100;
-    max_menu_width = 100;
-    documentation = true;
+require'compe'.setup {
+    enabled = true,
+    autocomplete = true,
+    debug = false,
+    min_length = 1,
+    preselect = 'enable',
+    throttle_time = 80,
+    source_timeout = 200,
+    incomplete_delay = 400,
+    max_abbr_width = 100,
+    max_kind_width = 100,
+    max_menu_width = 100,
+    documentation = true,
 
-    source = {
-      path = true;
-      buffer = true;
-      calc = true;
-      nvim_lsp = true;
-      nvim_lua = true;
-      vsnip = true;
-      ultisnips = true;
-    };
-  }
-  map('i', '<silent><expr> <Tab>', "compe#complete()")
-  map('i', '<silent><expr> <CR>',      "compe#confirm('<CR>')")
-  map('i', '<silent><expr> <C-e>',     "compe#close('<C-e>')")
-  map('i', '<silent><expr> <C-f>',     "compe#scroll({ 'delta': +4 })")
-  map('i', '<silent><expr> <C-d>',     "compe#scroll({ 'delta': -4 })")
+    source = {path = true, buffer = true, calc = true, nvim_lsp = true, nvim_lua = true, vsnip = true, ultisnips = true}
+}
+map('i', '<silent><expr> <Tab>', "compe#complete()")
+map('i', '<silent><expr> <CR>', "compe#confirm('<CR>')")
+map('i', '<silent><expr> <C-e>', "compe#close('<C-e>')")
+map('i', '<silent><expr> <C-f>', "compe#scroll({ 'delta': +4 })")
+map('i', '<silent><expr> <C-d>', "compe#scroll({ 'delta': -4 })")
 
-paq {'nvim-treesitter/nvim-treesitter', ['do']=':TSUpdate'}
+paq {'nvim-treesitter/nvim-treesitter', ['do'] = ':TSUpdate'}
 paq {'nvim-treesitter/nvim-treesitter-textobjects'}
 paq {'nvim-treesitter/playground'}
-  local ts = require 'nvim-treesitter.configs'
-  ts.setup {
-    ensure_installed = 'maintained',
-    highlight = {
-      enable = true
-    },
-    textobjects = {
-      enable = true,
-    },
-    incremental_selection = {
-      enable = true,
-    },
-  }
+local ts = require 'nvim-treesitter.configs'
+ts.setup {ensure_installed = 'maintained', highlight = {enable = true}, textobjects = {enable = true}, incremental_selection = {enable = true}}
 
 -- Git stuff!
 paq {'tpope/vim-fugitive'}
@@ -181,7 +137,7 @@ paq {'tpope/vim-unimpaired'}
 
 -- Fuzzy-find everything!
 paq {'junegunn/fzf.vim'}
-    vim.cmd([[
+vim.cmd([[
     nnoremap ; :Buffers<CR>
     nnoremap <C-p> :Files<CR>
 
@@ -195,7 +151,7 @@ paq {'junegunn/fzf.vim'}
     nnoremap <Leader>/ :History/<CR>
     "nnoremap <C-r> :Rg<CR>
     "cabbrev tabe Files<CR>
-    ]])
+]])
 
 -- Format tables and calculate stuff in them.
 paq {'dhruvasagar/vim-table-mode'}
@@ -210,50 +166,15 @@ paq {'kana/vim-textobj-user'}
 
 -- Snippets, duh!
 paq {'SirVer/ultisnips'}
-    vim.g.UltiSnipsExpandTrigger = "<Tab>"
-    vim.g.UltiSnipsJumpForwardTrigger = "<Tab>"
-    vim.g.UltiSnipsJumpBackwardTrigger = "<S-Tab>"
+vim.g.UltiSnipsExpandTrigger = "<Tab>"
+vim.g.UltiSnipsJumpForwardTrigger = "<Tab>"
+vim.g.UltiSnipsJumpBackwardTrigger = "<S-Tab>"
 
 -- In Ruby, insert 'end' statements automatically.
 paq {'tpope/vim-endwise'}
 
 -- Dead simple personal wiki plugin.
 paq {'blinry/vimboy'}
-
--- Asynchronous code linting and fixing
---paq {'w0rp/ale'}
---    let g:ale_linters = {
---    \    'rust': ['cargo'],
---    \    'glsl': ['glslang']
---    \}
---    let g:ale_fixers = {
---    \   'c': ['clang-format'],
---    \   'arduino': ['clang-format'],
---    \   'glsl': ['clang-format'],
---    \   'css': ['prettier'],
---    \   'haskell': ['hindent'],
---    \   'html': ['prettier'],
---    \   'json': ['prettier'],
---    \   'javascript': ['prettier'],
---    \   'vue': ['prettier']
---    \}
---    "\   'svelte': ['prettier'],
---    "\   'ruby': ['rufo'],
---    "\   'python': ['black'],
---
---    "let g:ale_linters_aliases = {
---    "\}
---    let g:ale_linters_explicit = 1
---    let g:ale_fix_on_save = 1
---    " Mappings in the style of unimpaired-next
---    nmap <silent> [W <Plug>(ale_first)
---    nmap <silent> [w <Plug>(ale_previous)
---    nmap <silent> ]w <Plug>(ale_next)
---    nmap <silent> ]W <Plug>(ale_last)
-
--- Asynchronous builds
---paq {'tpope/vim-dispatch'}
---paq {'radenling/vim-dispatch-neovim'}
 
 -- Observe EditorConfig coding style settings.
 paq {'editorconfig/editorconfig-vim'}
@@ -278,16 +199,16 @@ paq {'907th/vim-auto-save'}
 vim.cmd("colorscheme velvetopia")
 vim.opt.termguicolors = true -- Use 'gui' highlight attributes instead of cterm.
 vim.opt.list = true -- Show whitespace characters...
-vim.opt.listchars = { tab = '› ', trail = '·', extends = '❭', precedes = '❬' } -- ... like this!
+vim.opt.listchars = {tab = '› ', trail = '·', extends = '❭', precedes = '❬'} -- ... like this!
 vim.cmd("au InsertLeave * :set listchars+=trail:·") -- Show trailing whitespace in all modes...
 vim.cmd("au InsertEnter * :set listchars-=trail:·") -- ... except in insert mode.
-vim.opt.showbreak ="↳" -- Display this character in front of wrapped lines.
+vim.opt.showbreak = "↳" -- Display this character in front of wrapped lines.
 
 -- Backup, undo, swap.
 vim.opt.swapfile = false -- Don't keep swap files.
 vim.opt.undofile = true -- Keep undo files.
 vim.opt.backupcopy = "yes" -- When saving, make a copy, then overwrite the original file.
-                              -- This is required for parcel's Hot Module Reload.
+-- This is required for parcel's Hot Module Reload.
 vim.g.netrw_dirhistmax = 0 -- Don't write a .netrwhist file.
 
 -- Search.
@@ -317,7 +238,7 @@ vim.opt.mouse = "a" -- Enable mouse use in all modes.
 vim.opt.timeout = false -- Don't timeout on mappings.
 vim.opt.startofline = false -- Keep cursor in the same column for many commands.
 vim.opt.shell = '/bin/bash'
-vim.opt.clipboard = { 'unnamed' , 'unnamedplus' } -- Use * and + in yank/paste operations.
+vim.opt.clipboard = {'unnamed', 'unnamedplus'} -- Use * and + in yank/paste operations.
 vim.opt.lazyredraw = true -- Redraw only when we need to.
 vim.opt.splitright = true -- Open vertical splits on the right side.
 vim.opt.splitbelow = true -- Open horizontal splits on the bottom
@@ -343,9 +264,7 @@ map('v', '<C-j>', ']egv')
 map('n', '<Leader><Space>', '<Cmd>lua vim.o.hlsearch = not vim.o.hlsearch<CR>')
 
 -- Add more undo-steps when writing prose.
-for _, v in pairs({'.', '?', '!', ','}) do
-  map('i', v, v..'<C-g>u')
-end
+for _, v in pairs({'.', '?', '!', ','}) do map('i', v, v .. '<C-g>u') end
 
 -- Underline the current line with - or =.
 map('n', '<Leader>-', '"ayy"ap:s/./-/g<CR>')
