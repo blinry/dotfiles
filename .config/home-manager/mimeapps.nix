488536d27ff5ead1819a1edc13b86b7693de4cb8
blinry <mail@blinry.org>
2023-12-05 10:41:34 +0100

    [home manager] Add more mime associations

--- .config/home-manager/mimeapps.nix
+++ .config/home-manager/mimeapps.nix
@@ -1,11 +1,16 @@
 let
   associations = {
+    "nvim" = [ "text/plain" ];
     "org.gnome.eog" = [ "image/png" "image/jpeg" "image/gif" ];
     "org.inkscape.Inkscape" = [ "image/svg+xml" ];
     "mpv" = [ "video/mp4" "video/mpeg" ];
     "org.gnome.Evince" = [ "application/pdf" ];
     "firefox" = [ "text/html" "x-scheme-handler/http" "x-scheme-handler/https" ];
     "org.qbittorrent.qBittorrent" = [ "x-scheme-handler/magnet" ];
+    "com.github.johnfactotum.Foliate" = [ "application/epub+zip" ];
+    "writer" = [ "application/vnd.oasis.opendocument.text" ];
+    "calc" = [ "application/vnd.oasis.opendocument.spreadsheet" ];
+    "impress" = [ "application/vnd.oasis.opendocument.presentation" ];
   };
 in
 builtins.listToAttrs (builtins.concatMap
